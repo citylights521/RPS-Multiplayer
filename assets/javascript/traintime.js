3b6400c98f8e2da82377721afad74357d4e1b2d1
CGibeau <claire.gibeau@outlook.com>
2019-03-13 16:05:28 -0700

    wip

---
+++ assets/javascript/traintime.js
@@ -0,0 +1,16 @@
+//Javascript
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDcvFYLnDxb_jg-DkKElQSh2KIGNDbagbA",
+    authDomain: "fir-project-772f9.firebaseapp.com",
+    databaseURL: "https://fir-project-772f9.firebaseio.com",
+    projectId: "fir-project-772f9",
+    storageBucket: "fir-project-772f9.appspot.com",
+    messagingSenderId: "790740042122"
+  };
+  firebase.initializeApp(config);
+
+  
+var database = firebase.database;
